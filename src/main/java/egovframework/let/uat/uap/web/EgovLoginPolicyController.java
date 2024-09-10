bd331250d02f6ac2152a659461e9632c625cf0d6
이백행 <dlqorgod@naver.com>
2024-09-11 06:55:14 +0900

    Source > Format

--- src/main/java/egovframework/let/uat/uap/web/EgovLoginPolicyController.java
+++ src/main/java/egovframework/let/uat/uap/web/EgovLoginPolicyController.java
@@ -1,16 +1,9 @@
 package egovframework.let.uat.uap.web;
 
-import egovframework.com.cmm.EgovMessageSource;
-import egovframework.com.cmm.LoginVO;
-import egovframework.let.uat.uap.service.EgovLoginPolicyService;
-import egovframework.let.uat.uap.service.LoginPolicy;
-import egovframework.let.uat.uap.service.LoginPolicyVO;
+import javax.annotation.Resource;
 
 import org.egovframe.rte.fdl.security.userdetails.util.EgovUserDetailsHelper;
 import org.egovframe.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
-
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -20,16 +13,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springmodules.validation.commons.DefaultBeanValidator;
 
+import egovframework.com.cmm.EgovMessageSource;
+import egovframework.com.cmm.LoginVO;
+import egovframework.let.uat.uap.service.EgovLoginPolicyService;
+import egovframework.let.uat.uap.service.LoginPolicy;
+import egovframework.let.uat.uap.service.LoginPolicyVO;
+
 /**
+ * <pre>
  * 로그인정책에 대한 controller 클래스를 정의한다.
  * 로그인정책에 대한 등록, 수정, 삭제, 조회, 반영확인 기능을 제공한다.
  * 로그인정책의 조회기능은 목록조회, 상세조회로 구분된다.
+ * </pre>
+ * 
  * @author 공통서비스개발팀 lee.m.j
  * @since 2009.08.03
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자           수정내용
@@ -37,7 +39,7 @@ import org.springmodules.validation.commons.DefaultBeanValidator;
  *   2009.08.03  lee.m.j        최초 생성
  *   2011.08.31  JJY            경량환경 템플릿 커스터마이징버전 생성
  *
- * </pre>
+ *      </pre>
  */
 @Controller
 public class EgovLoginPolicyController {
@@ -53,6 +55,7 @@ public class EgovLoginPolicyController {
 
 	/**
 	 * 로그인정책 목록 조회화면으로 이동한다.
+	 * 
 	 * @return String - 리턴 Url
 	 */
 	@RequestMapping("/uat/uap/selectLoginPolicyListView.do")
@@ -62,11 +65,13 @@ public class EgovLoginPolicyController {
 
 	/**
 	 * 로그인정책 목록을 조회한다.
+	 * 
 	 * @param loginPolicyVO - 로그인정책 VO
 	 * @return String - 리턴 Url
 	 */
 	@RequestMapping("/uat/uap/selectLoginPolicyList.do")
-	public String selectLoginPolicyList(@ModelAttribute("loginPolicyVO") LoginPolicyVO loginPolicyVO, ModelMap model) throws Exception {
+	public String selectLoginPolicyList(@ModelAttribute("loginPolicyVO") LoginPolicyVO loginPolicyVO, ModelMap model)
+			throws Exception {
 
 		/** paging */
 		PaginationInfo paginationInfo = new PaginationInfo();
@@ -91,11 +96,13 @@ public class EgovLoginPolicyController {
 
 	/**
 	 * 로그인정책 목록의 상세정보를 조회한다.
+	 * 
 	 * @param loginPolicyVO - 로그인정책 VO
 	 * @return String - 리턴 Url
 	 */
 	@RequestMapping("/uat/uap/getLoginPolicy.do")
-	public String selectLoginPolicy(@RequestParam("emplyrId") String emplyrId, @ModelAttribute("loginPolicyVO") LoginPolicyVO loginPolicyVO, ModelMap model) throws Exception {
+	public String selectLoginPolicy(@RequestParam("emplyrId") String emplyrId,
+			@ModelAttribute("loginPolicyVO") LoginPolicyVO loginPolicyVO, ModelMap model) throws Exception {
 
 		loginPolicyVO.setEmplyrId(emplyrId);
 
@@ -112,11 +119,13 @@ public class EgovLoginPolicyController {
 
 	/**
 	 * 로그인정책 정보 등록화면으로 이동한다.
+	 * 
 	 * @param loginPolicy - 로그인정책 model
 	 * @return String - 리턴 Url
 	 */
 	@RequestMapping("/uat/uap/addLoginPolicyView.do")
-	public String insertLoginPolicyView(@RequestParam("emplyrId") String emplyrId, @ModelAttribute("loginPolicyVO") LoginPolicyVO loginPolicyVO, ModelMap model) throws Exception {
+	public String insertLoginPolicyView(@RequestParam("emplyrId") String emplyrId,
+			@ModelAttribute("loginPolicyVO") LoginPolicyVO loginPolicyVO, ModelMap model) throws Exception {
 
 		loginPolicyVO.setEmplyrId(emplyrId);
 
@@ -128,13 +137,15 @@ public class EgovLoginPolicyController {
 
 	/**
 	 * 로그인정책 정보를 신규로 등록한다.
+	 * 
 	 * @param loginPolicy - 로그인정책 model
 	 * @return String - 리턴 Url
 	 */
 	@RequestMapping("/uat/uap/addLoginPolicy.do")
-	public String insertLoginPolicy(@ModelAttribute("loginPolicy") LoginPolicy loginPolicy, BindingResult bindingResult, ModelMap model) throws Exception {
+	public String insertLoginPolicy(@ModelAttribute("loginPolicy") LoginPolicy loginPolicy, BindingResult bindingResult,
+			ModelMap model) throws Exception {
 
-		beanValidator.validate(loginPolicy, bindingResult); //validation 수행
+		beanValidator.validate(loginPolicy, bindingResult); // validation 수행
 
 		if (bindingResult.hasErrors()) {
 			model.addAttribute("loginPolicyVO", loginPolicy);
@@ -153,13 +164,15 @@ public class EgovLoginPolicyController {
 
 	/**
 	 * 기 등록된 로그인정책 정보를 수정한다.
+	 * 
 	 * @param loginPolicy - 로그인정책 model
 	 * @return String - 리턴 Url
 	 */
 	@RequestMapping("/uat/uap/updtLoginPolicy.do")
-	public String updateLoginPolicy(@ModelAttribute("loginPolicy") LoginPolicy loginPolicy, BindingResult bindingResult, ModelMap model) throws Exception {
+	public String updateLoginPolicy(@ModelAttribute("loginPolicy") LoginPolicy loginPolicy, BindingResult bindingResult,
+			ModelMap model) throws Exception {
 
-		beanValidator.validate(loginPolicy, bindingResult); //validation 수행
+		beanValidator.validate(loginPolicy, bindingResult); // validation 수행
 
 		if (bindingResult.hasErrors()) {
 			model.addAttribute("loginPolicyVO", loginPolicy);
@@ -177,11 +190,13 @@ public class EgovLoginPolicyController {
 
 	/**
 	 * 기 등록된 로그인정책 정보를 삭제한다.
+	 * 
 	 * @param loginPolicy - 로그인정책 model
 	 * @return String - 리턴 Url
 	 */
 	@RequestMapping("/uat/uap/removeLoginPolicy.do")
-	public String deleteLoginPolicy(@ModelAttribute("loginPolicy") LoginPolicy loginPolicy, ModelMap model) throws Exception {
+	public String deleteLoginPolicy(@ModelAttribute("loginPolicy") LoginPolicy loginPolicy, ModelMap model)
+			throws Exception {
 
 		egovLoginPolicyService.deleteLoginPolicy(loginPolicy);
 
