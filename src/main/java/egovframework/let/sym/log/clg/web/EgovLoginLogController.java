c6a709fec3b4830b0cd0a1b8d8187dae81a80138
이백행 <dlqorgod@naver.com>
2024-09-10 10:04:53 +0900

    접속로그관리 검색 조건 유지 (#16)
    * 접속로그관리 검색 조건 유지

--- src/main/java/egovframework/let/sym/log/clg/web/EgovLoginLogController.java
+++ src/main/java/egovframework/let/sym/log/clg/web/EgovLoginLogController.java
@@ -2,37 +2,38 @@ package egovframework.let.sym.log.clg.web;
 
 import java.util.HashMap;
 
-import egovframework.let.sym.log.clg.service.EgovLoginLogService;
-import egovframework.let.sym.log.clg.service.LoginLog;
+import javax.annotation.Resource;
 
 import org.egovframe.rte.fdl.property.EgovPropertyService;
 import org.egovframe.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
-
-import javax.annotation.Resource;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import egovframework.let.sym.log.clg.service.EgovLoginLogService;
+import egovframework.let.sym.log.clg.service.LoginLog;
 
 /**
  * 접속로그정보를 관리하기 위한 컨트롤러 클래스
+ * 
  * @author 공통서비스개발팀 이삼섭
  * @since 2009.03.11
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.03.11  이삼섭          최초 생성
  *   2011.07.01  이기하          패키지 분리(sym.log -> sym.log.clg)
- *   2011.08.31  JJY            경량환경 템플릿 커스터마이징버전 생성
+ *   2011.08.31  JJY           경량환경 템플릿 커스터마이징버전 생성
+ *   2024.09.06  이백행          컨트리뷰션 검색 조건 유지
  *
- * </pre>
+ *      </pre>
  */
 @Controller
 public class EgovLoginLogController {
@@ -50,7 +51,7 @@ public class EgovLoginLogController {
 	 * @return sym/log/clg/EgovLoginLogList
 	 * @throws Exception
 	 */
-	@RequestMapping(value = "/sym/log/clg/SelectLoginLogList.do")
+	@GetMapping(value = "/sym/log/clg/SelectLoginLogList.do")
 	public String selectLoginLogInf(@ModelAttribute("searchVO") LoginLog loginLog, ModelMap model) throws Exception {
 		System.out.println("eeee:::" + loginLog);
 		loginLog.setPageUnit(propertyService.getInt("pageUnit"));
@@ -87,8 +88,9 @@ public class EgovLoginLogController {
 	 * @return sym/log/clg/EgovLoginLogInqire
 	 * @throws Exception
 	 */
-	@RequestMapping(value = "/sym/log/clg/InqireLoginLog.do")
-	public String selectLoginLog(@ModelAttribute("searchVO") LoginLog loginLog, @RequestParam("logId") String logId, ModelMap model) throws Exception {
+	@GetMapping(value = "/sym/log/clg/InqireLoginLog.do")
+	public String selectLoginLog(@ModelAttribute("searchVO") LoginLog loginLog, @RequestParam("logId") String logId,
+			ModelMap model) throws Exception {
 
 		loginLog.setLogId(logId.trim());
 
