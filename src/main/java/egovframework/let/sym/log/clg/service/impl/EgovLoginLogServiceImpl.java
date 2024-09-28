422c6164f429845da84a49fcb4200acee06387bf
이백행 <dlqorgod@naver.com>
2024-09-28 15:40:10 +0900

    Source > Format

--- src/main/java/egovframework/let/sym/log/clg/service/impl/EgovLoginLogServiceImpl.java
+++ src/main/java/egovframework/let/sym/log/clg/service/impl/EgovLoginLogServiceImpl.java
@@ -3,43 +3,42 @@ package egovframework.let.sym.log.clg.service.impl;
 import java.util.HashMap;
 import java.util.Map;
 
-import egovframework.let.sym.log.clg.service.EgovLoginLogService;
-import egovframework.let.sym.log.clg.service.LoginLog;
+import javax.annotation.Resource;
 
 import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
 import org.egovframe.rte.fdl.idgnr.EgovIdGnrService;
-
-import javax.annotation.Resource;
-
 import org.springframework.stereotype.Service;
+
+import egovframework.let.sym.log.clg.service.EgovLoginLogService;
+import egovframework.let.sym.log.clg.service.LoginLog;
 
 /**
  * 접속로그 관리를 위한 서비스 구현 클래스
+ * 
  * @author 공통서비스개발팀 이삼섭
  * @since 2009.03.11
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *   
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.03.11  이삼섭          최초 생성
  *   2011.07.01  이기하          패키지 분리(stm.log -> sym.log.clg)
- *   2011.08.31  JJY            경량환경 템플릿 커스터마이징버전 생성 
+ *   2011.08.31  JJY            경량환경 템플릿 커스터마이징버전 생성
  *
- * </pre>
+ *      </pre>
  */
 @Service("EgovLoginLogService")
-public class EgovLoginLogServiceImpl extends EgovAbstractServiceImpl implements
-	EgovLoginLogService {
+public class EgovLoginLogServiceImpl extends EgovAbstractServiceImpl implements EgovLoginLogService {
 
-	@Resource(name="loginLogDAO")
-	private LoginLogDAO loginLogDAO;	
-	
-    /** ID Generation */    
-	@Resource(name="egovLoginLogIdGnrService")
+	@Resource(name = "loginLogDAO")
+	private LoginLogDAO loginLogDAO;
+
+	/** ID Generation */
+	@Resource(name = "egovLoginLogIdGnrService")
 	private EgovIdGnrService egovLoginLogIdGnrService;
 
 	/**
@@ -47,12 +46,13 @@ public class EgovLoginLogServiceImpl extends EgovAbstractServiceImpl implements
 	 * 
 	 * @param LoginLog
 	 */
+	@Override
 	public void logInsertLoginLog(LoginLog loinLog) throws Exception {
 		// TODO Auto-generated method stub
 		String logId = egovLoginLogIdGnrService.getNextStringId();
 		loinLog.setLogId(logId);
-		
-		loginLogDAO.logInsertLoginLog(loinLog);    	
+
+		loginLogDAO.logInsertLoginLog(loinLog);
 
 	}
 
@@ -61,25 +61,27 @@ public class EgovLoginLogServiceImpl extends EgovAbstractServiceImpl implements
 	 * 
 	 * @param loginLog
 	 * @return loginLog
-	 * @throws Exception 
+	 * @throws Exception
 	 */
-	public LoginLog selectLoginLog(LoginLog loginLog) throws Exception{
-		
+	@Override
+	public LoginLog selectLoginLog(LoginLog loginLog) throws Exception {
+
 		return loginLogDAO.selectLoginLog(loginLog);
-	}	
+	}
 
 	/**
 	 * 접속로그 목록을 조회한다.
 	 * 
 	 * @param LoginLog
 	 */
+	@Override
 	public Map<String, Object> selectLoginLogInf(LoginLog loinLog) throws Exception {
 		// TODO Auto-generated method stub
-		 
+
 		Map<String, Object> _map = new HashMap<String, Object>();
 		_map.put("resultList", loginLogDAO.selectLoginLogInf(loinLog));
 		_map.put("resultCnt", Integer.toString(loginLogDAO.selectLoginLogInfCnt(loinLog)));
-		 
+
 		return _map;
 	}
 
