af8605722746af30459f6d704717f7027fca0e15
이백행 <dlqorgod@naver.com>
2024-09-28 18:11:06 +0900

    Source > Format

--- src/main/java/egovframework/let/sym/mnu/mcm/service/impl/EgovMenuCreateManageServiceImpl.java
+++ src/main/java/egovframework/let/sym/mnu/mcm/service/impl/EgovMenuCreateManageServiceImpl.java
@@ -2,24 +2,24 @@ package egovframework.let.sym.mnu.mcm.service.impl;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import org.springframework.stereotype.Service;
+
 import egovframework.com.cmm.ComDefaultVO;
 import egovframework.let.sym.mnu.mcm.service.EgovMenuCreateManageService;
 import egovframework.let.sym.mnu.mcm.service.MenuCreatVO;
 
-import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
-
-import javax.annotation.Resource;
-
-import org.springframework.stereotype.Service;
-
 /**
  * 메뉴목록, 사이트맵 생성을 처리하는 비즈니스 구현 클래스를 정의한다.
+ * 
  * @author 개발환경 개발팀 이용
  * @since 2009.06.01
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자           수정내용
@@ -28,7 +28,7 @@ import org.springframework.stereotype.Service;
  *   2011.07.01  서준식          EgovMenuManageServiceImpl에서 메뉴 생성 관련 부분 분리
  *   2011.08.31  JJY            경량환경 템플릿 커스터마이징버전 생성
  *
- * </pre>
+ *      </pre>
  */
 @Service("meunCreateManageService")
 public class EgovMenuCreateManageServiceImpl extends EgovAbstractServiceImpl implements EgovMenuCreateManageService {
@@ -38,6 +38,7 @@ public class EgovMenuCreateManageServiceImpl extends EgovAbstractServiceImpl imp
 
 	/**
 	 * ID 존재여부를 조회
+	 * 
 	 * @param vo ComDefaultVO
 	 * @return int
 	 * @exception Exception
@@ -49,7 +50,8 @@ public class EgovMenuCreateManageServiceImpl extends EgovAbstractServiceImpl imp
 
 	/**
 	 * 메뉴생성 내역을 조회
-	 * @param  vo MenuCreatVO
+	 * 
+	 * @param vo MenuCreatVO
 	 * @return List
 	 * @exception Exception
 	 */
@@ -60,7 +62,8 @@ public class EgovMenuCreateManageServiceImpl extends EgovAbstractServiceImpl imp
 
 	/**
 	 * 화면에 조회된 메뉴정보로 메뉴생성내역 데이터베이스에서 입력
-	 * @param checkedAuthorForInsert  String
+	 * 
+	 * @param checkedAuthorForInsert String
 	 * @param checkedMenuNoForInsert String
 	 * @exception Exception
 	 */
@@ -88,6 +91,7 @@ public class EgovMenuCreateManageServiceImpl extends EgovAbstractServiceImpl imp
 
 	/**
 	 * 메뉴생성관리 목록을 조회
+	 * 
 	 * @param vo ComDefaultVO
 	 * @return List
 	 * @exception Exception
@@ -99,6 +103,7 @@ public class EgovMenuCreateManageServiceImpl extends EgovAbstractServiceImpl imp
 
 	/**
 	 * ID에 대한 권한코드를 조회
+	 * 
 	 * @param vo ComDefaultVO
 	 * @return MenuCreatVO
 	 * @exception Exception
@@ -110,6 +115,7 @@ public class EgovMenuCreateManageServiceImpl extends EgovAbstractServiceImpl imp
 
 	/**
 	 * 메뉴생성관리 총건수를 조회한다.
+	 * 
 	 * @param vo ComDefaultVO
 	 * @return int
 	 * @exception Exception
