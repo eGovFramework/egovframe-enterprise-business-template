6699f866b7d8a7d7ea81f5fb2e06ca470bb7fab0
이백행 <dlqorgod@naver.com>
2024-09-30 14:00:31 +0900

    [공지사항] 롬복 생성자 기반 종속성 주입 (#49)
    * [공지사항] 롬복 생성자 기반 종속성 주입

--- src/main/java/egovframework/let/cop/bbs/web/EgovBBSManageController.java
+++ src/main/java/egovframework/let/cop/bbs/web/EgovBBSManageController.java
@@ -4,13 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.egovframe.rte.fdl.property.EgovPropertyService;
 import org.egovframe.rte.fdl.security.userdetails.util.EgovUserDetailsHelper;
 import org.egovframe.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -52,7 +50,7 @@ import lombok.RequiredArgsConstructor;
  *   2009.06.29  한성곤          2단계 기능 추가 (댓글관리, 만족도조사)
  *   2011.08.31  JJY           경량환경 템플릿 커스터마이징버전 생성
  *   2024.08.24  이백행          요청 메서드 정리
- *   2024.09.28  이백행          컨트리뷰션 롬복 생성자 기반 종속성 주입
+ *   2024.09.29  이백행          컨트리뷰션 롬복 생성자 기반 종속성 주입 *
  *
  *      </pre>
  */
@@ -64,20 +62,15 @@ public class EgovBBSManageController {
 
 	private final EgovBBSAttributeManageService bbsAttrbService;
 
-	@Resource(name = "EgovFileMngService")
-	private EgovFileMngService fileMngService;
+	private final EgovFileMngService fileMngService;
 
-	@Resource(name = "EgovFileMngUtil")
-	private EgovFileMngUtil fileUtil;
+	private final EgovFileMngUtil fileUtil;
 
-	@Resource(name = "propertiesService")
-	protected EgovPropertyService propertyService;
+	private final EgovPropertyService propertyService;
 
-	@Resource(name = "egovMessageSource")
-	EgovMessageSource egovMessageSource;
+	private final EgovMessageSource egovMessageSource;
 
-	@Autowired
-	private DefaultBeanValidator beanValidator;
+	private final DefaultBeanValidator beanValidator;
 
 	/**
 	 * XSS 방지 처리.
