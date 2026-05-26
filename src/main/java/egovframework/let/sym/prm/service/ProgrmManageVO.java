68c9f0c8c4e0b5cc93e987611b6079b4abd70d99
dasomel <dasomell@gmail.com>
2026-05-27 01:47:01 +0900

    refactor: sym 모듈 VO에 Lombok @Getter/@Setter 적용
    sym/cal, sym/ccm, sym/prm 패키지 7개 VO의 수동 getter/setter를
    Lombok 어노테이션으로 대체하여 코드 중복을 제거한다.
    pom.xml maven-compiler-plugin에 annotationProcessorPaths 추가.
    
    대상 파일:
    - RestdeVO, CmmnCodeVO, CmmnClCodeVO, CmmnDetailCodeVO, ZipVO
    - ProgrmManageVO, ProgrmManageDtlVO

--- src/main/java/egovframework/let/sym/prm/service/ProgrmManageVO.java
+++ src/main/java/egovframework/let/sym/prm/service/ProgrmManageVO.java
@@ -2,8 +2,10 @@ package egovframework.let.sym.prm.service;
 
 import org.egovframe.rte.ptl.reactive.validation.EgovNullCheck;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
-/** 
+/**
  * 프로그램목록 처리를 위한 VO 클래스르를 정의한다
  * @author 개발환경 개발팀 이용
  * @since 2009.06.01
@@ -12,7 +14,7 @@ import jakarta.validation.constraints.Size;
  *
  * <pre>
  * << 개정이력(Modification Information) >>
- *   
+ *
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.03.20  이  용          최초 생성
@@ -20,8 +22,9 @@ import jakarta.validation.constraints.Size;
  *
  * </pre>
  */
-
-public class ProgrmManageVO{
+@Getter
+@Setter
+public class ProgrmManageVO {
 
 	/** 프로그램파일명 */
 	@EgovNullCheck
@@ -39,78 +42,7 @@ public class ProgrmManageVO{
 	@EgovNullCheck
 	@Size(max=60)
 	private String URL;
-	/** 프로그램설명	 */	
+	/** 프로그램설명 */
 	private String progrmDc;
 
-	/**
-	 * progrmFileNm attribute를 리턴한다.
-	 * @return String
-	 */
-	public String getProgrmFileNm() {
-		return progrmFileNm;
-	}
-	/**
-	 * progrmFileNm attribute 값을 설정한다.
-	 * @param progrmFileNm String
-	 */
-	public void setProgrmFileNm(String progrmFileNm) {
-		this.progrmFileNm = progrmFileNm;
-	}
-	/**
-	 * progrmStrePath attribute를 리턴한다.
-	 * @return String
-	 */
-	public String getProgrmStrePath() {
-		return progrmStrePath;
-	}
-	/**
-	 * progrmStrePath attribute 값을 설정한다.
-	 * @param progrmStrePath String
-	 */
-	public void setProgrmStrePath(String progrmStrePath) {
-		this.progrmStrePath = progrmStrePath;
-	}
-	/**
-	 * progrmKoreanNm attribute를 리턴한다.
-	 * @return String
-	 */
-	public String getProgrmKoreanNm() {
-		return progrmKoreanNm;
-	}
-	/**
-	 * progrmKoreanNm attribute 값을 설정한다.
-	 * @param progrmKoreanNm String
-	 */
-	public void setProgrmKoreanNm(String progrmKoreanNm) {
-		this.progrmKoreanNm = progrmKoreanNm;
-	}
-	/**
-	 * url attribute를 리턴한다.
-	 * @return String
-	 */
-	public String getURL() {
-		return URL;
-	}
-	/**
-	 * URL attribute 값을 설정한다.
-	 * @param URL String
-	 */
-	public void setURL(String URL) {
-		this.URL = URL;
-	}
-	/**
-	 * progrmDc attribute를 리턴한다.
-	 * @return String
-	 */
-	public String getProgrmDc() {
-		return progrmDc;
-	}
-	/**
-	 * progrmDc attribute 값을 설정한다.
-	 * @param progrmDc String
-	 */
-	public void setProgrmDc(String progrmDc) {
-		this.progrmDc = progrmDc;
-	}
-  
 }
