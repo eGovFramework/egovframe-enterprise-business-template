e2b578b239ea4667f255a4a5facc27ef29b4a6d8
dasomel <dasomell@gmail.com>
2026-05-28 00:05:58 +0900

    refactor: sec 모듈 모델/VO에 Lombok @Getter/@Setter 적용
    sec/gmt, sec/ram, sec/rgm, sec/rmt 패키지의 모델 및 VO 클래스
    수동 getter/setter를 Lombok 어노테이션으로 대체하여 코드 중복을 제거한다.
    pom.xml maven-compiler-plugin에 annotationProcessorPaths 추가.
    
    대상 파일:
    - GroupManage, GroupManageVO
    - AuthorManage, AuthorManageVO
    - AuthorRoleManage, AuthorRoleManageVO
    - AuthorGroup, AuthorGroupVO
    - RoleManage, RoleManageVO

--- src/main/java/egovframework/let/sec/rmt/service/RoleManage.java
+++ src/main/java/egovframework/let/sec/rmt/service/RoleManage.java
@@ -3,6 +3,8 @@ package egovframework.let.sec.rmt.service;
 import org.egovframe.rte.ptl.reactive.validation.EgovNullCheck;
 import jakarta.validation.constraints.Size;
 import egovframework.com.cmm.ComDefaultVO;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * 롤관리에 대한 model 클래스를 정의한다.
@@ -13,15 +15,17 @@ import egovframework.com.cmm.ComDefaultVO;
  *
  * <pre>
  * << 개정이력(Modification Information) >>
- *   
+ *
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.03.20  이문준          최초 생성
- *   2011.08.31  JJY            경량환경 템플릿 커스터마이징버전 생성 
+ *   2011.08.31  JJY            경량환경 템플릿 커스터마이징버전 생성
  *
  * </pre>
  */
 
+@Getter
+@Setter
 public class RoleManage extends ComDefaultVO {
 	/**
 	 * serialVersionUID
@@ -40,23 +44,23 @@ public class RoleManage extends ComDefaultVO {
 	 */
 	@EgovNullCheck
 	@Size(max=50)
-	private String roleNm;	
+	private String roleNm;
 	/**
 	 * 롤패턴
 	 */
 	@EgovNullCheck
 	@Size(max=50)
-	private String rolePtn;	
+	private String rolePtn;
 	/**
 	 * 롤 설명
 	 */
 	@Size(max=50)
-	private String roleDc;		
+	private String roleDc;
 	/**
 	 * 롤 타입
 	 */
 	@EgovNullCheck
-	private String roleTyp;		
+	private String roleTyp;
 	/**
 	 * 롤 Sort
 	 */
@@ -71,133 +75,5 @@ public class RoleManage extends ComDefaultVO {
 	 * 권한 코드
 	 */
 	private String authorCode;
-	
-	/**
-	 * roleManage attribute 를 리턴한다.
-	 * @return RoleManage
-	 */
-	public RoleManage getRoleManage() {
-		return roleManage;
-	}
-	/**
-	 * roleManage attribute 값을 설정한다.
-	 * @param roleManage RoleManage 
-	 */
-	public void setRoleManage(RoleManage roleManage) {
-		this.roleManage = roleManage;
-	}
-	/**
-	 * roleCode attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getRoleCode() {
-		return roleCode;
-	}
-	/**
-	 * roleCode attribute 값을 설정한다.
-	 * @param roleCode String 
-	 */
-	public void setRoleCode(String roleCode) {
-		this.roleCode = roleCode;
-	}
-	/**
-	 * roleNm attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getRoleNm() {
-		return roleNm;
-	}
-	/**
-	 * roleNm attribute 값을 설정한다.
-	 * @param roleNm String 
-	 */
-	public void setRoleNm(String roleNm) {
-		this.roleNm = roleNm;
-	}
-	/**
-	 * rolePtn attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getRolePtn() {
-		return rolePtn;
-	}
-	/**
-	 * rolePtn attribute 값을 설정한다.
-	 * @param rolePtn String 
-	 */
-	public void setRolePtn(String rolePtn) {
-		this.rolePtn = rolePtn;
-	}
-	/**
-	 * roleDc attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getRoleDc() {
-		return roleDc;
-	}
-	/**
-	 * roleDc attribute 값을 설정한다.
-	 * @param roleDc String 
-	 */
-	public void setRoleDc(String roleDc) {
-		this.roleDc = roleDc;
-	}
-	/**
-	 * roleTyp attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getRoleTyp() {
-		return roleTyp;
-	}
-	/**
-	 * roleTyp attribute 값을 설정한다.
-	 * @param roleTyp String 
-	 */
-	public void setRoleTyp(String roleTyp) {
-		this.roleTyp = roleTyp;
-	}
-	/**
-	 * roleSort attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getRoleSort() {
-		return roleSort;
-	}
-	/**
-	 * roleSort attribute 값을 설정한다.
-	 * @param roleSort String 
-	 */
-	public void setRoleSort(String roleSort) {
-		this.roleSort = roleSort;
-	}
-	/**
-	 * roleCreatDe attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getRoleCreatDe() {
-		return roleCreatDe;
-	}
-	/**
-	 * roleCreatDe attribute 값을 설정한다.
-	 * @param roleCreatDe String 
-	 */
-	public void setRoleCreatDe(String roleCreatDe) {
-		this.roleCreatDe = roleCreatDe;
-	}
-	/**
-	 * authorCode attribute 를 리턴한다.
-	 * @return String
-	 */
-	public String getAuthorCode() {
-		return authorCode;
-	}
-	/**
-	 * authorCode attribute 값을 설정한다.
-	 * @param authorCode String 
-	 */
-	public void setAuthorCode(String authorCode) {
-		this.authorCode = authorCode;
-	}
-	
 
 }
