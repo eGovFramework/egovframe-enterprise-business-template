4128ae1f3e1d08e0355e299500f13344b65df47a
이백행 <dlqorgod@naver.com>
2026-06-20 10:16:01 +0900

    chore: AuthorGroupDAO 불필요한 예외(throws Exception) 제거

--- src/main/java/egovframework/let/sec/rgm/service/impl/AuthorGroupDAO.java
+++ src/main/java/egovframework/let/sec/rgm/service/impl/AuthorGroupDAO.java
@@ -22,6 +22,7 @@ import egovframework.let.sec.rgm.service.AuthorGroupVO;
  *  -------    --------    ---------------------------
  *   2009.03.20  이문준          최초 생성
  *   2011.08.31  JJY            경량환경 템플릿 커스터마이징버전 생성
+ *   2026.06.20  이백행          [2026년 컨트리뷰션] 불필요한 예외(throws Exception) 제거
  *
  * </pre>
  */
@@ -33,36 +34,32 @@ public class AuthorGroupDAO extends EgovAbstractMapper {
 	 * 그룹별 할당된 권한 목록 조회
 	 * @param authorGroupVO AuthorGroupVO
 	 * @return List<AuthorGroupVO>
-	 * @exception Exception
 	 */
-	public List<AuthorGroupVO> selectAuthorGroupList(AuthorGroupVO authorGroupVO) throws Exception {
+	public List<AuthorGroupVO> selectAuthorGroupList(AuthorGroupVO authorGroupVO) {
 		return selectList("authorGroupDAO.selectAuthorGroupList", authorGroupVO);
 	}
 
 	/**
 	 * 그룹에 권한정보를 할당하여 데이터베이스에 등록
 	 * @param authorGroup AuthorGroup
-	 * @exception Exception
 	 */
-	public void insertAuthorGroup(AuthorGroup authorGroup) throws Exception {
+	public void insertAuthorGroup(AuthorGroup authorGroup) {
 		insert("authorGroupDAO.insertAuthorGroup", authorGroup);
 	}
 
 	/**
 	 * 화면에 조회된 그룹권한정보를 수정하여 항목의 정합성을 체크하고 수정된 데이터를 데이터베이스에 반영
 	 * @param authorGroup AuthorGroup
-	 * @exception Exception
 	 */
-	public void updateAuthorGroup(AuthorGroup authorGroup) throws Exception {
+	public void updateAuthorGroup(AuthorGroup authorGroup) {
 		update("authorGroupDAO.updateAuthorGroup", authorGroup);
 	}
 
 	/**
 	 * 그룹별 할당된 시스템 메뉴 접근권한을 삭제
 	 * @param authorGroup AuthorGroup
-	 * @exception Exception
 	 */
-	public void deleteAuthorGroup(AuthorGroup authorGroup) throws Exception {
+	public void deleteAuthorGroup(AuthorGroup authorGroup) {
 		delete("authorGroupDAO.deleteAuthorGroup", authorGroup);
 	}
 
@@ -70,9 +67,8 @@ public class AuthorGroupDAO extends EgovAbstractMapper {
 	 * 그룹권한목록 총 갯수를 조회한다.
 	 * @param authorGroupVO AuthorGroupVO
 	 * @return int
-	 * @exception Exception
 	 */
-    public int selectAuthorGroupListTotCnt(AuthorGroupVO authorGroupVO) throws Exception {
+    public int selectAuthorGroupListTotCnt(AuthorGroupVO authorGroupVO) {
         return (Integer)selectOne("authorGroupDAO.selectAuthorGroupListTotCnt", authorGroupVO);
     }
 }
